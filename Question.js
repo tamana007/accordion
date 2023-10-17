4d8515320f2987f85e309da0b3e3e496175020a3
tamana007 <tamana.efatwira1@gmail.com>
2023-10-17 12:49:25 -0700

    my practice

--- Question.js
+++ Question.js
@@ -4,12 +4,27 @@ import questions from './data';
 
 
 const Question = () => {
+  const [question,setQuestion]=useState()
+  const [click,setClick]=useState(false);
 
-  const {id,title,info}=questions;
+  // const {id,title,info}=ques;
+  function handleBtn(personId){
+    setClick(true);
+    setQuestion(personId)
+    // console.log(question);
+   
+    
+  }
+
   
   return <article className='container'><h2>question component</h2>;
-  {questions.map((question)=>{
-    return <article>{questions.id}</article>
+  {questions.map(({id,title,info})=>{
+    return <article className='question'>{title}
+     <button className='btn' onClick={handleBtn}>click</button> 
+     {setClick && setQuestion===id && <p> Answer: {info}</p>}</article>
+   
+   
+   
     
 
   })}
